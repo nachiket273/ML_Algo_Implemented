644a8bb07fee420e56167354a9792f902b968d72
nachiket273 <nachiket.tanksale@gmail.com>
2019-11-14 00:09:10 +0530

    Added logic to return mean from predict when classifier is set to False i.e. it's regression problem.

--- KNN/kNN.py
+++ KNN/kNN.py
@@ -1,20 +1,27 @@
 import numpy as np
+from sklearn.base import BaseEstimator
 
 def get_eucliedean_dist(x, y):
     return np.sqrt(np.sum(y**2, axis=1).reshape(y.shape[0], 1) \
     + np.sum(x**2, axis=1) - 2 * y.dot(x.T))
 
 
-class kNN():
+class kNN(BaseEstimator):
     """
     Implementation of k-nearest neighbor algorithm.
 
     Args:
-        k   Number of neighbors to be considered for prediction.
-            Default: 5
+        k           Number of neighbors to be considered for prediction.
+                    Default: 5
+
+        classifier  Boolean variable to decide if it's a classifier or regressor.
+                    If false, the predict function will return mean instead of 
+                    majority class.
+                    Default: True
     """
-    def __init__(self, k=5):
+    def __init__(self, k=5, classifier=True):
         self.k = k
+        self.classifier = classifier
 
     def fit(self, X, y):
         assert(self.k < X.shape[0])
@@ -32,12 +39,11 @@ class kNN():
         # use these idx to find labels
         for i, idx in enumerate(idxs):
             labels = self.y[idx]
-            cnts = np.bincount(labels.reshape(labels.shape[0]))
-            preds.append(np.argmax(cnts))
+            if self.classifier:
+                cnts = np.bincount(labels.reshape(labels.shape[0]))
+                preds.append(np.argmax(cnts))
+            else:
+                mean = np.mean(labels.reshape(labels.shape[0]))
+                preds.append(mean)
 
         return preds
-
-    def set_params(self, **parameters):
-        for parameter, value in parameters.items():
-            setattr(self, parameter, value)
-        return self
