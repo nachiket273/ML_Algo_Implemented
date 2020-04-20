733772c9043b0f9fe43ff58c1207de65beba4805
nachiket273 <nachiket.tanksale@gmail.com>
2020-04-21 01:13:01 +0530

    Gradient Boost Implementation with simple notebook

--- GradientBoosting/GradientBoost.py
+++ GradientBoosting/GradientBoost.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator
-from Loss import CrossEntropyLoss, MSE
+from Loss import DevianceLoss, MSE
 
 import sys
 import os
@@ -9,63 +9,83 @@ sys.path.append(os.path.abspath('../DecisionTree'))
 from DecisionTree import DecisionTree
 
 class GradientBoost(BaseEstimator):
-    def __init__(self, n_estimators, learning_rate=0.1, max_features=0, 
-                    max_depth=np.inf, min_samples_split=2, 
+    def __init__(self, n_estimators, learning_rate=0.1, criterion='mse', 
+                    max_features=0, 
+                    max_depth=3, min_samples_split=2, 
                     is_classification=True, random_seed=0):
         self.n_estimators = n_estimators
         self.trees = []
         self.learning_rate = learning_rate
         self.max_features = max_features
         self.max_depth = max_depth
+        self.criterion = criterion
+        assert(criterion == 'mse' or criterion == 'mae')
         self.min_samples_split = min_samples_split
         self.random_seed = random_seed
         self.is_classification = is_classification
 
         if self.is_classification:
-            self.loss = CrossEntropyLoss()
+            self.loss = DevianceLoss()
         else:
             self.loss = MSE()
-
-        for _ in range(self.n_estimators):
-            self.trees.append(DecisionTree(max_depth=self.max_depth, 
-                                max_features=self.max_features,
-                                min_samples_split = self.min_samples_split,
-                                criterion = 'mse',
-                                random_seed = self.random_seed
-                            ))
 
     def fit(self, X, y):
         np.random.seed(self.random_seed)
         if isinstance(X, pd.DataFrame):
             X = X.to_numpy()
-        y_pred = np.full(X.shape[0], np.mean(y, axis=0))
-        for i in range(self.n_estimators):
-            gradients = self.loss.grad(y, y_pred)
-
-            # Now fit the regression tree with this gradient as 
-            # target variables.
-            self.trees[i].fit(X, gradients)
-
-            # Make predictions on same training data
-            preds = self.trees[i].predict(X)
-
-            # Now use predictions and learning rate to mimimize
-            # error.
-            y_pred -= np.multiply(self.learning_rate, preds)
-
-    def predict(self, X_test):
-        y_preds = np.array([])
-        for i,_ in enumerate(self.trees):
-            preds = self.trees[i].predict(X_test)
-            if y_preds.any():
-                y_preds -= np.multiply(self.learning_rate, preds)
-            else:
-                y_preds = -np.multiply(self.learning_rate, preds)
 
         if self.is_classification:
-            y_preds = np.expand_dims(y_preds, axis=1)
-            exp_preds = np.exp(y_preds)
-            exp_preds = exp_preds/ np.expand_dims(np.sum(exp_preds, axis=1), axis=1)
-            y_preds = np.argmax(exp_preds, axis=1)
+            classes = list(np.unique(y))
+            self.n_classes = len(classes)
+            for j, clss in enumerate(classes):
+                trees = []
+                init = False
+                for i in range(self.n_estimators):
+                    y_true = y == clss
+                    y_true = y_true.astype(int)
+                    if not init:
+                        y_pred = np.zeros(X.shape[0], np.float32)
+                    gradients = self.loss.grad(y_true, y_pred, k=j)
+                    tree = DecisionTree(max_depth=self.max_depth, 
+                                max_features=self.max_features,
+                                min_samples_split = self.min_samples_split,
+                                criterion = self.criterion,
+                                random_seed = self.random_seed
+                            )
+                    tree.fit(X, gradients)
+                    preds = tree.predict(X)
+                    y_pred -= np.multiply(self.learning_rate, preds)
+                    trees.append(tree)
+                self.trees.append(trees)
+        else:
+            y_pred = np.zeros(X.shape[0])
+            for i in range(self.n_estimators):
+                gradients = self.loss.grad(y, y_pred)
+                tree = DecisionTree(max_depth=self.max_depth, 
+                                max_features=self.max_features,
+                                min_samples_split = self.min_samples_split,
+                                criterion = self.criterion,
+                                random_seed = self.random_seed
+                            )
+                tree.fit(X, gradients)
+                preds = tree.predict(X)
+                y_pred -= np.multiply(self.learning_rate, preds)
+                self.trees.append(tree)
+
+    def predict(self, X_test):
+        if self.is_classification:
+            y_preds = np.zeros((X_test.shape[0], self.n_classes), np.float32)
+            for i, _ in enumerate(self.trees):
+                y_pred = np.zeros(X_test.shape[0], np.float32)
+                for tr in self.trees[i]:
+                    preds = tr.predict(X_test)
+                    y_pred -= np.multiply(self.learning_rate, preds)
+                y_preds[:, i] = y_pred
+            y_preds = np.argmax(y_preds, axis=1)
+        else:
+            y_preds = np.zeros(X_test.shape[0], np.float32)
+            for i,_ in enumerate(self.trees):
+                preds = self.trees[i].predict(X_test)
+                y_preds -= np.multiply(self.learning_rate, preds)
 
         return y_preds
---
+++ GradientBoosting/Loss.py
@@ -0,0 +1,30 @@
+import numpy as np
+from scipy.special import logsumexp
+
+class Loss(object):
+    def loss(self, y_true, y_pred):
+        raise NotImplementedError()
+
+    def grad(self, y_true, y_pred):
+        raise NotImplementedError()
+
+class DevianceLoss(Loss):
+    def loss(self, y_true, y_pred):
+        n_classes = len(np.unique(y_true))
+
+        Y = np.zeros((y_true.shape[0], n_classes), dtype=np.float64)
+        for i in range(n_classes):
+            Y[:, i] = y_true == i
+
+        return np.average(-1 * (y_true * y_pred).sum(axis=1) + logsumexp(y_pred, axis=1))
+
+    def grad(self, y_true, y_pred, k=0):
+        return -1.0 * y_true + np.nan_to_num(np.exp(y_pred -
+                                        logsumexp(y_pred)))
+
+class MSE(Loss):
+    def loss(self, y_true, y_pred):
+        return 0.5 * np.mean((y_true - y_pred.ravel()) ** 2.0)
+
+    def grad(self, y_true, y_pred):
+        return -(y_true - y_pred.ravel())
